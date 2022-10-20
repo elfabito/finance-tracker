5f6cb78707009543b4d8c0308eda37f33690cc98
Fab <info@desane.com.uy>
2022-10-20 17:19:40 -0300

    Display stock objects in browser

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,7 +3,7 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://cloud.iexapis.com/v1')
-  client.quote(ticker_symbol).latest_price
+  new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price)
   end
 
 end
