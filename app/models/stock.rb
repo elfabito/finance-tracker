bd3c0531b99aaae034cbbb5fc78e5f20e8f20acc
Fab <info@desane.com.uy>
2022-10-20 17:39:39 -0300

    Invalid search alerts

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -3,7 +3,11 @@ class Stock < ApplicationRecord
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(publishable_token: Rails.application.credentials.iex_client[:sandbox_api_key],
                                   endpoint: 'https://cloud.iexapis.com/v1')
-  new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price)
+    begin
+      new(ticker: ticker_symbol, name: client.company(ticker_symbol).company_name, last_price: client.quote(ticker_symbol).latest_price)
+    rescue => exception
+      return nil
+    end
   end
 
 end
